4c41ba9b82b21b6ea1e1007424778d89daa72b1a
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2017-03-26 23:43:05 +0500

    feat(*): added main classes for the future game

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,48 @@
 'use strict';
+/** Imports */
+// Import global dependencies. (I remind you, webpack create separate chunk file for them).
+require('pixi');    // Because Phaser use PIXI and p2 as global variables, they must be imported first.
+require('p2');
+require('phaser');  // So, in my case, TypeScript breaks if i import it as `import 'phaser';`. ¯\_(ツ)_/¯
+
+import 'styles/style.styl'; // Registering styles for the page; They will automatically inject.
+
+
+// The main state of the game
+export class MainState extends Phaser.State {
+  preload(): void { }
+
+  create(): void { }
+
+  update(): void { }
+}
+
+
+// The main class of our application
+export default class App extends Phaser.Game {
+  constructor(config: Phaser.IGameConfig) {
+    super(config);
+
+    this.state.add('main', MainState); // Add `main` state into game
+
+    this.state.start('main'); // Initialize and start `main` state
+  }
+}
+
+
+// Like python's `__name__ == "__main__"` checks whether the module is part
+// of another program or it is executable.
+if (!module.parent) {
+  window.onload = () => {
+    const config: Phaser.IGameConfig = {
+      width:           800, // width of canvas
+      height:          600, // height of canvas
+      renderer:        Phaser.AUTO, // rendering context. The recommended parameter is Phaser.AUTO
+      parent:          '',
+      resolution:      1,
+      forceSetTimeOut: false // tell Phaser to use `setTimeOut` even if `requestAnimationFrame` is available.
+    };
+
+    new App(config); // Initialize the application. It will automatically inject <canvas /> into <body />
+  };
+}
