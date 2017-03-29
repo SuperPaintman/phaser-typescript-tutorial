18886874d3d6ee8ce3654df96efaf22dc7285eaa
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2017-03-30 02:44:24 +0500

    feat(states): added drawing text

--- src/states/main.state.ts
+++ src/states/main.state.ts
@@ -5,6 +5,20 @@ import State from './state';
 // The main state of the game
 export default class MainState extends State {
   create(): void {
-    this.game.add.sprite(0, 0, 'star');
+    const map = [
+      ' XXX  XXX XXXXX X  X X   X XXXX ',
+      'X      X    X   X  X X   X X   X',
+      'X  XX  X    X   XXXX X   X XXXX ',
+      'X   X  X    X   X  X X   X X   X',
+      ' XXX  XXX   X   X  X  XXX  XXXX '
+    ].map((line) => line.split(''));
+
+    map.forEach((line, y) => line.forEach((char, x) => {
+      if (char !== 'X') {
+        return;
+      }
+
+      this.game.add.sprite(x * 24, y * 22, 'star');
+    }));
   }
 }
