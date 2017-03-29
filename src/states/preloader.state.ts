7106244334d0e403b2f92122e3e27f98dba6b23a
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2017-03-30 02:42:38 +0500

    feat(states,assets): added assets

--- src/states/preloader.state.ts
+++ src/states/preloader.state.ts
@@ -2,11 +2,22 @@
 /** Imports */
 import State from './state';
 
+// Webpack will replace these imports with a URLs to images
+const skyImage        = require('assets/images/sky.png'); // const skyImage = '/assets/images/sky.png';
+const platformImage   = require('assets/images/platform.png');
+const starImage       = require('assets/images/star.png');
+const dudeImage       = require('assets/images/dude.png');
+
 
 // The state for loading core resources for the game
 export default class PreloaderState extends State {
   preload(): void {
     console.debug('Assets loading started');
+
+    this.game.load.image('sky', skyImage);
+    this.game.load.image('platform', platformImage);
+    this.game.load.image('star', starImage);
+    this.game.load.spritesheet('dude', dudeImage, 32, 48);
   }
 
   create(): void {
